5b7034fa47a6a10e802ae59e788b9e1db0323c8b
Anna Putina <arputina@gmail.com>
2024-03-22 21:49:22 +0100

    Attempt to chains with participants

--- dataset-explore-chains.py
+++ dataset-explore-chains.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import json
 
 def is_nan(value):
     return isinstance(value, float) and np.isnan(value)
@@ -22,7 +23,7 @@ def generate_key(email):
     #sender and who recieves the message
     participants = sorted(participants)
     key = ' '.join(participants)
-    key = ' '.join([email['Subject'], key])
+    key = ' '.join([email['Subject'] + ' | ', key])
     # removing spaces and unwated char from beggining of the key
     #We remove "Re:" because we want the same identification for both send \
     #and reply
@@ -49,43 +50,37 @@ def chain_info_from_frame(frame):
     return chain_identifier_vs_length
 
 ##################################################################
+if __name__ == "__main__":
+    
+    # Uploading data
+    file_path = 'data/no-identical-mails-data.csv'
+    df = pd.read_csv(file_path)
 
-file_path = 'data/parsed-mails-data-allen-p.csv'
-df = pd.read_csv(file_path)
+    # Impossible to build a chain without knowinf the recepients
+    df = df.dropna(subset=['To'])
+    print(f"Missing values in the dataframe:\n{df.isna().sum()}\n")
 
-df = df.dropna(subset=['To'])
+    # Splitting email adresses
+    df['From'] = df['From'].map(split_email_addresses)
+    df['To'] = df['To'].map(split_email_addresses)
+    df['Cc'] = df['Cc'].map(split_email_addresses)
+    df['Bcc'] = df['Bcc'].map(split_email_addresses)
 
-df.head()
-df.shape
+    print(f"\nThe dataset's size after cleaning: {df.shape}\n")
+    print(f"\nFirst rows after cleaning:\n{df.head()}\n")
 
-set(df["From"][0])
+    chains = chain_info_from_frame(df)
 
-df['From'] = df['From'].map(split_email_addresses)
-df['To'] = df['To'].map(split_email_addresses)
-df['Cc'] = df['Cc'].map(split_email_addresses)
-df['Bcc'] = df['Bcc'].map(split_email_addresses)
+    print(f"Number of chains detected: {len(chains)}")
 
-my_set = {'apple', 'banana', 'orange'}
+    # Savin results
+    with open('data/chains.json', 'w') as file:
+        json.dump(chains, file)
 
-# Convert set elements to strings and join them
-result = ', '.join(my_set)
+    # Filter the data for chains with length greater than 4
+    filtered_chains = {key: value for key, value in chains.items() if value['length'] > 4}
 
-# Print rows where 'To' column contains NaN
-nan_rows = df[df['To'].isna()]
-
-print("Rows with NaN in 'To' column:")
-print(nan_rows)
-
-df.isna().sum()
-
-generate_key(df.iloc[0,:])
-
-email = df.iloc[0,:]
-email['From']
-
-set(email['From'])
-
-
-chains = chain_info_from_frame(df)
-
-len(chains)
+    with open('data/chains_greater_4.json', "w") as json_file:
+        json.dump(filtered_chains, json_file, indent=4)
+        
+    print("\nChains files created successfully!")
