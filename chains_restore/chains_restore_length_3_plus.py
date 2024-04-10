e83b99c85b0812d7972edbe7e27d6593023d58d4
Anna Putina <arputina@gmail.com>
2024-04-10 05:26:39 +0200

    First version chains 3+

--- chains_restore/chains_restore_length_3_plus.py
+++ chains_restore/chains_restore_length_3_plus.py
@@ -65,6 +65,57 @@ def has_fwd_prefix(string):
     prefix_pattern = re.compile(r'(Fw:|Fwd:)', flags=re.IGNORECASE)
     match = re.search(prefix_pattern, string)
     return bool(match)
+        
+def add_clean_text(text):
+    '''Cleans the text content for later processing, removing unnecessary characters and spaces.
+    
+    Args:
+    - text (str): Text content to be cleaned.
+    
+    Returns:
+    - str: Cleaned text content.
+    '''
+    # Remove the symbols
+    text = re.sub(r'=20', ' ', text, flags=re.IGNORECASE)
+    text = re.sub(r'=09', ' ', text, flags=re.IGNORECASE)
+    text = re.sub(r'=018', ' ', text, flags=re.IGNORECASE)
+    text = re.sub(r'=01', ' ', text, flags=re.IGNORECASE)
+
+    text = re.sub(r'_!', ' ', text, flags=re.IGNORECASE)
+    text = re.sub(r'_', ' ', text, flags=re.IGNORECASE)
+    text = re.sub(r'\*', ' ', text, flags=re.IGNORECASE)
+    text = re.sub(r'\?', ' ', text, flags=re.IGNORECASE)
+    text = re.sub(r'~', ' ', text, flags=re.IGNORECASE)
+    text = re.sub(r'3D', ' ', text, flags=re.IGNORECASE)
+    
+    text = re.sub(r'\+', '', text, flags=re.IGNORECASE)
+    text = re.sub(r'"', '', text, flags=re.IGNORECASE)
+    text = re.sub(r'&', '', text, flags=re.IGNORECASE)
+    text = re.sub(r'=\n', '', text, flags=re.IGNORECASE)
+    text = re.sub(r'=', '', text, flags=re.IGNORECASE)
+    
+    text = re.sub(r'<Embedded StdOleLink>', ' ', text, flags=re.IGNORECASE)
+    text = re.sub(r'<Embedded Picture (Metafile)>', ' ', text, flags=re.IGNORECASE)
+    text = re.sub(r'<Embedded >', ' ', text, flags=re.IGNORECASE)
+    text = re.sub(r'<Embedded Picture (Device Independent Bitmap)>', ' ', text, flags=re.IGNORECASE)
+
+    # Clean up other unnecessary characters and spaces
+    text = re.sub(r'[\n\t]+', ' ', text)
+    text = re.sub(r'\s{2,}', ' ', text)
+    text = text.strip()
+    
+    return text.lower()
+
+def contains_specific_phrases(text):
+    '''Checks if the text contains specific phrases case-insensitively using regular expressions.
+
+    Args:
+    - text (str): The text to be checked.
+
+    Returns:
+    - bool: True if the text contains any of the specified phrases (case-insensitive), False otherwise.
+    '''
+    return bool(re.search(r'Forwarded by|Original Message|\(Revision: \d\)', text, flags=re.IGNORECASE))
 
 def print_groups(dictionary, key, combined_path):
     '''
@@ -118,6 +169,11 @@ if __name__ == "__main__":
         
     df = pd.read_csv('../'+paths.DATA_CLEAN_SUBJECT)
     df.set_index('file', inplace=True)
+    
+    # Count the number of NaNs for each column
+    nan_counts = df.isna().sum()
+    print(f"Number of NaNs for every column:\n{nan_counts}")
+    df['content-clean'] = df['content-clean'].fillna('')
 
     # Split email addresses in the DataFrame
     df['From'] = df['From'].map(split_email_addresses)
@@ -198,8 +254,96 @@ if __name__ == "__main__":
                 chains[key]['length'].append(len(candidate))
                 chains[key]['chains'].append(candidate)
         if len(chains[key]['chains']) == 0:
-            del chains[key]       
+            del chains[key]      
             
+  
+    chains = {}
+    new = []
+    for key, value in list(ordered_groups.items()):
+        chains[key]={}
+        chains[key]['length'] = []
+        chains[key]['chains'] = []
+        emails_list = (ordered_groups[key]['ids']).copy()
+        while len(emails_list) > 1:
+            candidate = []
+            candidate.append(emails_list.pop(0))
+            emails_list_cpy = emails_list.copy()
+            for email in emails_list_cpy:
+                candidate_cpy = candidate.copy()
+                for item in candidate_cpy:
+                    # Time period regulation
+                    if abs(df.loc[email, 'date-timestamp'] - df.loc[item, 'date-timestamp']) > (60**2)*24*30*2:
+                        continue 
+                    # Re/Fwd neither in the first email, nor in the second (follow-ups are difficult 
+                    # to detect, they could be mixed up with 2 separate emails with the same subject)
+                    if not (df.loc[email, 're'] or df.loc[item, 're'] or df.loc[email, 'fwd'] or df.loc[item, 'fwd']):
+                        # Shorter time to make sure it's a chain
+                        if abs(df.loc[email, 'date-timestamp'] - df.loc[item, 'date-timestamp']) > (60**2)*24*7:
+                            continue
+                        # Forward and (maybe?) Reply
+                        if (df.loc[item, 'From'] == df.loc[email, 'From']) and (df.loc[item, 'content-clean'] in df.loc[email, 'content-clean']) and contains_specific_phrases(df.loc[email, 'content-clean']):
+                            if df.loc[item, 'content-clean'] != df.loc[email, 'content-clean']:
+                                candidate.append(email)
+                                emails_list.remove(email)
+                                break
+                        # Reply or Forward
+                        if df.loc[email, 'From'].intersection(df.loc[item, 'recepients'].difference(df.loc[item, 'From'])):
+                            candidate.append(email)
+                            emails_list.remove(email)
+                            break       
+                    # Re/Fwd in the second email
+                    elif (df.loc[email, 'fwd'] or df.loc[email, 're']):
+                        # Reply or Forward
+                        if df.loc[email, 'From'].intersection(df.loc[item, 'recepients'].difference(df.loc[item, 'From'])):
+                            candidate.append(email)
+                            emails_list.remove(email)
+                            break
+                        # Another case of Forward
+                        if (df.loc[item, 'From'] == df.loc[email, 'From']) and df.loc[email, 'fwd']:
+                            if df.loc[item, 'content-clean'] != df.loc[email, 'content-clean']:
+                                candidate.append(email)
+                                emails_list.remove(email)
+                                break
+                        # Follow-up
+                        if (df.loc[item, 'From'] == df.loc[email, 'From']) and ((df.loc[email, 'recepients'].difference(df.loc[email, 'From'])).intersection(df.loc[item, 'recepients'].difference(df.loc[item, 'From']))):
+                            if df.loc[item, 'content-clean'] != df.loc[email, 'content-clean']:
+                                # Avoiding time errors emails
+                                if (abs(df.loc[item, 'date-timestamp'] - df.loc[email, 'date-timestamp'])/(60*60)) % 1 == 0:
+                                    if (add_clean_text(df.loc[item, 'content-clean']) == add_clean_text(df.loc[email, 'content-clean'])) or (df.loc[item, 'Content-Type'] != df.loc[email, 'Content-Type']):
+                                        continue  
+                                candidate.append(email)
+                                emails_list.remove(email)
+                                break
+                    # Wrong Timing
+                    elif (df.loc[item, 'fwd'] or df.loc[item, 're']):
+                        # Shorter time to make sure it's a chain
+                        if abs(df.loc[item, 'date-timestamp'] - df.loc[email, 'date-timestamp']) > (60**2)*8:
+                            continue
+                        # Reply or Forward
+                        if df.loc[item, 'From'].intersection(df.loc[email, 'recepients'].difference(df.loc[email, 'From'])):
+                            candidate.append(email)
+                            emails_list.remove(email)
+                            break
+                        # Another case of Forward
+                        if (df.loc[item, 'From'] == df.loc[email, 'From']) and df.loc[item, 'fwd']:
+                            if df.loc[item, 'content-clean'] != df.loc[email, 'content-clean']:
+                                candidate.append(email)
+                                emails_list.remove(email)
+                                break
+                        # Follow-up
+                        if (df.loc[item, 'From'] == df.loc[email, 'From']) and ((df.loc[item, 'recepients'].difference(df.loc[item, 'From'])).intersection(df.loc[email, 'recepients'].difference(df.loc[email, 'From']))):
+                            if df.loc[item, 'content-clean'] != df.loc[email, 'content-clean']:
+                                candidate.append(email)
+                                emails_list.remove(email)
+                                break 
+                    else:
+                        continue             
+            if len(candidate) > 1:
+                chains[key]['length'].append(len(candidate))
+                chains[key]['chains'].append(candidate)
+        if len(chains[key]['chains']) == 0:
+            del chains[key]                      
+                    
 
     total_chains = 0
     length_distribution = {}
